0261026f477cfc64241985177cf1e0f0afe5b5f6
Phumiphat Wongchuea <59161109@go.buu.ac.th>
2019-12-13 18:18:10 +0700

    finish

--- test-chai.js
+++ test-chai.js
@@ -1,34 +1,30 @@
-const chai = require('chai')
-const expect = chai.expect
-const math = require('./math')
+const chai = require('chai');
+const expect = chai.expect;
+const math = require('./math');
+describe('Test chai',()=>{
+    it('Should compare thing',()=>{
+        expect(1).to.equal(1);
+    });
+    it('Should compare anything',()=>{
+        expect(5>8).to.be.false;
+        expect({name: 'EIE'}).to.have.property('name').to.equal('EIE')
+        expect({}).to.be.a('object');
+        expect(1).to.be.a('number');
+        expect('EOE').to.be.a('string');
+        expect('EOE'.length).to.equal(3);
+        expect('EOE').to.lengthOf(3);
+        expect([1,2,4]).to.lengthOf(3);
+        expect(null).to.be.null;
+        expect(undefined).to.not.exist;
+        expect(1).to.exist; 
+    });
+});
 
-describe('Test chai', ()=>{
-    it('should compare thing by expect', () => {
-        expect(1).to.equal(1)
-    })
-    it('should compare another thing by expect', () => {
-        expect(5>8).to.be.false
-        expect({name: 'dome'}).to.deep.equal({name: 'dome'})
-        expect({name: 'dome'}).to.have.property('name').to.equal('dome')
-        expect({}.to.be.a('object'))
-        expect((1).to.be.a('number'))
-        expect('dome').to.be.a('string')
-        expect('dome'.length).to.equal(4)
-        expect('dome').to.lengthOf(4)        
-        expect([1,2,3]).to.lengthOf(3)
-        expect(null).to.but.null
-        expect(undefined).to.not.exist
-        expect(1).to.exist
-    })
-})
-
-describe('Math module',() =>{
-    context('Function add1' , () =>{
-        it('ควรส่งค่ากลับเป็นตัวเลข') , () =>{
-            expect(math.add(0,0)).to.be.a('number')
-        }
-        it('add(1,1) ควรส่งค่ากลับเป็น 2' , () =>{
-            expect(math.add(1,1)).to.equal(2)
-        })
-    })
-})
+describe('test math module',()=>{
+    it('ควรส่งค่ากลับเป็นตัวเลข',()=>{
+        expect(math.add1(1,1)).to.be.a('number');
+    });
+    it('ควรส่งค่ากลับเป็น 2',()=>{
+        expect(math.add1(1,1)).to.equal(2)
+    });
+});
