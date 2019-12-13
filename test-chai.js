6843bc3c1b1845880f5356d4e51c4dd5cc08a222
Phumiphat Wongchuea <59161109@go.buu.ac.th>
2019-12-13 18:19:54 +0700

    finish

--- test-chai.js
+++ test-chai.js
@@ -7,12 +7,12 @@ describe('Test chai',()=>{
     });
     it('Should compare anything',()=>{
         expect(5>8).to.be.false;
-        expect({name: 'EIE'}).to.have.property('name').to.equal('EIE')
+        expect({name: 'dom'}).to.have.property('name').to.equal('dom')
         expect({}).to.be.a('object');
         expect(1).to.be.a('number');
-        expect('EOE').to.be.a('string');
-        expect('EOE'.length).to.equal(3);
-        expect('EOE').to.lengthOf(3);
+        expect('dom').to.be.a('string');
+        expect('dom'.length).to.equal(3);
+        expect('dom').to.lengthOf(3);
         expect([1,2,4]).to.lengthOf(3);
         expect(null).to.be.null;
         expect(undefined).to.not.exist;
